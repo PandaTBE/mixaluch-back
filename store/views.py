10a03837418a2ff32ce3b346e3593d0511975b75
Nikita Surov <surov.n2012@gmail.com>
2022-11-17 22:07:45 +0300

    update gitignore

--- store/views.py
+++ store/views.py
@@ -1,7 +1,11 @@
+from django.http import HttpResponse, Http404
 from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import  Response
 
 from .models import Product
 from .serializers import ProductSerializer
+from categories.models import Category
 
 
 class ProductListView(generics.ListAPIView):
@@ -17,3 +21,22 @@ class SingleProduct(generics.RetrieveAPIView):
 class PopularProducts(generics.ListAPIView):
     queryset = Product.objects.filter(is_popular=True)
     serializer_class = ProductSerializer
+
+
+class ProductsByCategorySlug(generics.ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        slug = self.kwargs["slug"]
+        available_categories = []
+        try:
+            current_category = Category.objects.get(slug=slug)
+            available_categories.append(current_category)
+            if current_category.parent is None:
+                child_categories = Category.objects.filter(parent=current_category.id)
+                for category in child_categories:
+                    available_categories.append(category)
+        except:
+            raise Http404('Страница не найдена')
+
+        return Product.objects.filter(category__in=available_categories)
