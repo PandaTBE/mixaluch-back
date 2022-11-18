b4e1f1de704b52bc98d302da47c4a6a936e1a642
Nikita Surov <surov.n2012@gmail.com>
2022-11-18 18:10:25 +0300

    поменял логику получения товаров по категории

--- store/urls.py
+++ store/urls.py
@@ -7,6 +7,5 @@ app_name = "store"
 urlpatterns = [
     path("api/products/", views.ProductListView.as_view(), name="store_home"),
     path("api/products/<int:pk>/", views.SingleProduct.as_view(), name="product"),
-    path("api/products/<slug:slug>/", views.ProductsByCategorySlug.as_view(), name="product by category slug"),
     path("api/products/popular/", views.PopularProducts.as_view(), name="main page products"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,16 +1,33 @@
-from django.http import HttpResponse, Http404
-from rest_framework import generics
-from rest_framework import status
-from rest_framework.response import  Response
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+from categories.models import Category
+from django.http import Http404, HttpResponse
 
 from .models import Product
 from .serializers import ProductSerializer
-from categories.models import Category
 
 
 class ProductListView(generics.ListAPIView):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        category = self.request.GET.get("category", None)
+        if category is not None:
+            available_categories = []
+            try:
+                current_category = Category.objects.get(slug=category)
+                available_categories.append(current_category)
+                if current_category.parent is None:
+                    child_categories = Category.objects.filter(parent=current_category.id)
+                    for category in child_categories:
+                        available_categories.append(category)
+            except:
+                raise Http404("Страница не найдена")
+            return Product.objects.filter(category__in=available_categories)
+
+        else:
+            return Product.objects.all()
 
 
 class SingleProduct(generics.RetrieveAPIView):
@@ -21,22 +38,3 @@ class SingleProduct(generics.RetrieveAPIView):
 class PopularProducts(generics.ListAPIView):
     queryset = Product.objects.filter(is_popular=True)
     serializer_class = ProductSerializer
-
-
-class ProductsByCategorySlug(generics.ListAPIView):
-    serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        slug = self.kwargs["slug"]
-        available_categories = []
-        try:
-            current_category = Category.objects.get(slug=slug)
-            available_categories.append(current_category)
-            if current_category.parent is None:
-                child_categories = Category.objects.filter(parent=current_category.id)
-                for category in child_categories:
-                    available_categories.append(category)
-        except:
-            raise Http404('Страница не найдена')
-
-        return Product.objects.filter(category__in=available_categories)
