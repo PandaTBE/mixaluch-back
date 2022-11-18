925c844537beb21fba731dc7513163a266a3e60c
Nikita Surov <surov.n2012@gmail.com>
2022-11-18 18:11:17 +0300

    фикс

--- store/views.py
+++ store/views.py
@@ -1,8 +1,7 @@
 from rest_framework import generics, status
-from rest_framework.response import Response
 
 from categories.models import Category
-from django.http import Http404, HttpResponse
+from django.http import Http404
 
 from .models import Product
 from .serializers import ProductSerializer
