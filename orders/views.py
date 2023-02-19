87425744ac1727f128301de5f36dc9e03f17fad8
Nikita Surov <surov.n2012@gmail.com>
2023-02-19 18:15:54 +0300

    123

--- orders/views.py
+++ orders/views.py
@@ -13,7 +13,7 @@ class OrderListCreateAPIView(generics.ListCreateAPIView):
         user = self.request.user
         if not user.is_anonymous:
             return Order.objects.filter(user=user)
-        return Order.objects.all()
+        return []
 
     def perform_create(self, serializer):
         user = self.request.user
