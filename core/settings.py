09cfb8ec4f4d1bbe09988dfb4b2b592836a59d1f
Nikita Surov <surov.n2012@gmail.com>
2023-03-07 10:03:57 +0300

    123

--- core/settings.py
+++ core/settings.py
@@ -151,7 +151,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "mixaluch.alerts@gmail.com"
-EMAIL_HOST_PASSWORD = "zzjeqgarlqylusir"
+EMAIL_HOST_PASSWORD = "oioqfkacmbyaizfo"
 EMAIL_USE_TLS = True
 
 # Static files (CSS, JavaScript, Images)
