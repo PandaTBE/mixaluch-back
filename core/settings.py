aa3594fb82fa584c02e18d1d9fa09d096236a51e
Nikita Surov <surov.n2012@gmail.com>
2023-03-21 13:25:27 +0300

    123

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = MY_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = MY_DEBUG
+DEBUG = bool(int(MY_DEBUG))
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = MY_CSRF_TRUSTED_ORIGINS.split(' ')
